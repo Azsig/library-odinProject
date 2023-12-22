011a930114548f023aa547e0ae8d8a99a5ed99ec
Azsig <yasir_abdulaziz@yahoo.co.id>
2023-12-22 19:42:10 +0700

    add script to submit, cancel btn and form

--- script.js
+++ script.js
@@ -28,12 +28,34 @@ let page = document.querySelector('#page');
 let author = document.querySelector('#author');
 let year = document.querySelector('#year');
 
+function check(form){
+    if (form.value === ""){
+        form.classList.add('invalid')
+        return 1;
+    } 
+    else{
+        if(form.classList.contains('invalid')){
+            form.classList.remove('invalid')
+        }
+        return 0;
+    }
+}
+
 function addBookToLibrary(title, page, author, year){
-    theLibrary.push(book(title.value, author.value, page.value, year.value))
+    theLibrary.push(new book(title.value, author.value, page.value, year.value))
 }
 
 let submit = document.querySelector('#submit');
 
+let form = document.querySelectorAll('.form');
+
 submit.addEventListener('click', () =>{
-    addBookToLibrary(title, page, author, year);
+    if(check(title) === 1 || check(author) === 1 || check(year) === 1 || check(page) === 1){
+        return
+    }
+    else{
+        addBookToLibrary(title, page, author, year);
+        formBG.classList.add('hidden');
+    }
+    
 })
